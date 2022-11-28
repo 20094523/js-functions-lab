83e443726e89e563ebd898d8f3a96f0b420ebc79
Dominik Martynski <20094523@mail.wit.ie>
2022-11-28 19:59:42 +0000

    Exercise 2

---
+++ exercise2.js
@@ -0,0 +1,11 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(todos => {
+    const todoTitles = todos.map((todo) => todo.title)
+    console.log(todoTitles)
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
+
+
